2e56db2d2abd88589241b4dea2e5b6864cda49dc
ugurkorkmaz <korkmaz.uu@gmail.com>
2023-03-07 16:23:41 +0300

    feat(frontend): create message compenent

---
+++ template/src/lib/component/Message.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+    export let text: string
+</script>
+
+<div class="main">
+    <h1>{text}</h1>
+</div>
+
+<style lang="postcss">
+    .main{
+        @apply h-screen w-screen flex justify-center items-center;
+    }
+    .main h1 {
+        @apply font-semibold text-lg inline-flex justify-center items-center;
+    }
+</style>
---
+++ template/src/routes/+page.svelte
@@ -0,0 +1,5 @@
+<script>
+  import Message from "$lib/component/Message.svelte";
+
+</script>
+<Message text="Hello World" />
