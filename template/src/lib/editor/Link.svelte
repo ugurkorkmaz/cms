564320fde5edee4302a6d069e8f7d4952dc19bc9
ugurkorkmaz <korkmaz.uu@gmail.com>
2023-03-11 00:10:01 +0300

    feat: skeleton editor create

---
+++ template/src/lib/editor/Link.svelte
@@ -0,0 +1,7 @@
+<script>
+  import Icon from "$lib/component/Icon.svelte";
+</script>
+
+<button class="btn btn-circle">
+  <Icon name="add_link" type="filled" />
+</button>
---
+++ template/src/lib/editor/type.ts
@@ -0,0 +1,5 @@
+export type text = {
+    markdown: string,
+    html: string,
+    ast?: string
+}
